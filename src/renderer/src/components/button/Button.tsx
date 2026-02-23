7cbbb1877548e9f6973f3eedba445cf05f70c31e
devmlb <97614168+devmlb@users.noreply.github.com>
2026-02-23 20:44:33 +0100

    Added a button component

---
+++ src/renderer/src/components/button/Button.tsx
@@ -0,0 +1,111 @@
+import { cloneElement, JSX } from "react";
+
+import "./Button.css";
+
+function Button({
+    className,
+    id,
+    text,
+    icon,
+    iconOnly = false,
+    onClick = () => {},
+    secondary = false,
+    disabled = false,
+    loading = false,
+}: {
+    className: string | undefined;
+    id: string | undefined;
+    text: string | undefined;
+    icon: React.JSX.Element | undefined;
+    iconOnly: boolean;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
+    secondary: boolean;
+    disabled: boolean;
+    loading: boolean;
+}): JSX.Element {
+    let classes = "button";
+    classes += iconOnly ? " icon-button" : "";
+    classes += secondary ? " secondary" : " primary";
+    classes += loading ? " loading" : "";
+    classes += className ? ` ${className}` : "";
+
+    return (
+        <button
+            id={id}
+            disabled={disabled || loading}
+            className={classes}
+            onClick={onClick}
+        >
+            {icon ? cloneElement(icon, { size: 16, strokeWidth: 2.75 }) : null}
+            {text && <span>{text}</span>}
+        </button>
+    );
+}
+
+function IconButton({
+    className,
+    id,
+    icon,
+    onClick = () => {},
+    secondary = false,
+    disabled = false,
+    loading = false,
+}: {
+    className?: string;
+    id?: string;
+    icon: React.JSX.Element;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    secondary?: boolean;
+    disabled?: boolean;
+    loading?: boolean;
+}): JSX.Element {
+    return (
+        <Button
+            className={className}
+            id={id}
+            text={undefined}
+            icon={icon}
+            iconOnly={true}
+            onClick={onClick}
+            secondary={secondary}
+            disabled={disabled}
+            loading={loading}
+        />
+    );
+}
+
+function TextButton({
+    className,
+    id,
+    text,
+    icon,
+    onClick = () => {},
+    secondary = false,
+    disabled = false,
+    loading = false,
+}: {
+    className?: string;
+    id?: string;
+    text: string;
+    icon?: React.JSX.Element;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    secondary?: boolean;
+    disabled?: boolean;
+    loading?: boolean;
+}): JSX.Element {
+    return (
+        <Button
+            className={className}
+            id={id}
+            text={text}
+            icon={icon}
+            iconOnly={false}
+            onClick={onClick}
+            secondary={secondary}
+            disabled={disabled}
+            loading={loading}
+        />
+    );
+}
+
+export { TextButton, IconButton };
