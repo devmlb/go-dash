361bee1c9e15f833f8e7edf74b1171fb2f2a3c65
devmlb <97614168+devmlb@users.noreply.github.com>
2026-07-13 12:12:44 +0200

    The window is now starting with the last saved state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ use tauri_plugin_prevent_default::Flags;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
